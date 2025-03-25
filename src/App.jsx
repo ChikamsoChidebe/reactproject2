d360df885d3dd8c7d68f38ff23cf2c63e687aae4
Chikamso Chidebe <chikamsochidebe@gmail.com>
2025-03-25 13:50:49 +0100

    React Template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className=' flex flex-col width-[20px]'>
+      <input type="checkbox" name="" id="" /><input type="text" className=' border-[2px] width-[10px]' />
+      <input type="checkbox" name="" id="" /><input type="text" className=' border-[2px] width-[10px]' />
+      <input type="checkbox" name="" id="" /><input type="text" className=' border-[2px] width-[10px]' />
+    </div>
+  )
+}
+
+export default App
