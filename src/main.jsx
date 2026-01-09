c914b42171a418d972ca04c3f450589dc3a1004d
Chikamso Chidebe <chikamsochidebe@gmail.com>
2026-01-09 21:11:57 +0100

    Commit main

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './tailwind.css'
+import './styles.css'
 import App from './App'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from './Header'
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
-import Footer from './Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-    </Routes>
-    <Footer/>
-    </BrowserRouter> */}
     <App/>
   </StrictMode>
 )    
